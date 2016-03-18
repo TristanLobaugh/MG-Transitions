fb3a91f118e0602b30ba262628b1443ee7a3e181
TristanLobaugh <ttrader_atl@yahoo.com>
2016-03-18 17:14:55 -0400

    init commit

---
+++ JS/scripts.js
@@ -0,0 +1,111 @@
+$(document).ready(function(){
+	var moves = 0;
+	var wins = 0;
+	var games = 0;
+	var cards = [
+		"<img src='img/monsters-01.png'>",
+		"<img src='img/monsters-02.png'>",
+		"<img src='img/monsters-03.png'>",
+		"<img src='img/monsters-04.png'>",
+		"<img src='img/monsters-05.png'>",
+		"<img src='img/monsters-06.png'>",
+		"<img src='img/monsters-07.png'>",
+		"<img src='img/monsters-08.png'>",
+		"<img src='img/monsters-09.png'>",
+		"<img src='img/monsters-11.png'>",
+		"<img src='img/monsters-13.png'>",
+		"<img src='img/monsters-14.png'>",
+		"<img src='img/monsters-15.png'>",
+	];
+	var gridSize;
+	var gameTiles;
+	var gridArray;
+	var rowSize;
+
+	$("input").click(function(){
+		var dif = $(this).val();
+		if(dif == "easy"){
+			rowSize = 5;
+			gridSize = rowSize * 2;
+		}else if(dif == "med"){
+			rowSize = 5;
+			gridSize = rowSize * 4;
+		}else if(dif == "hard"){
+			rowSize = 7;
+			gridSize = rowSize * 4;
+			$("#mg-wrapper").addClass("hard");
+		}
+		$("#button-bucket").toggle();
+		gameTiles = cards.slice(0,(gridSize/2));
+		gridArray = $.merge(gameTiles, gameTiles);
+
+		//shuffle here
+
+		//place here
+		for(var i = 0; i < gridArray.length; i++){
+			var html = '<div class="mg-tile">';
+					html += '<div class="mg-tile-inner unmatched flipped">';
+						html += '<div class="mg-tile-outside"></div>';
+						html += '<div class="mg-tile-inside">' + gridArray[i] + '</div>';
+					html += '</div>';
+				html += '</div>';
+				$("#mg-contents").append(html);	
+		}
+		setTimeout(function(){
+			$('.mg-tile-inner').removeClass("flipped");
+		}, 2000);
+
+		$(".mg-tile").click(function(){
+			$(this).find(".mg-tile-inner").addClass("flipped");
+			if($(".flipped.unmatched").length == 2){
+				moves++;
+				var visibleCards = $(".flipped.unmatched img")
+				if(visibleCards[0].src == visibleCards[1].src){
+					$(".flipped.unmatched").addClass("matched");
+					$(".flipped.unmatched").removeClass("unmatched");
+				}else{
+					setTimeout(function(){
+					$(".flipped.unmatched").removeClass("flipped")
+				},500);
+				}
+				if($(".flipped.matched").length == gridArray.length){
+					alert("all matched");
+					wins++
+				}
+
+			}else{
+
+			}
+			$("#move-counter").html(moves);
+			$("#wins-counter").html(wins);
+		});
+
+
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
